ad580661e53d07203413ba22952cf46a569b7bca
NabeKz <ayuzak4me38@gmail.com>
2025-05-29 22:06:06 +0900

    Initialize new Rust web application with Actix-web framework
    - Created Cargo.toml to define package metadata and dependencies including actix-web, chrono, and serde.
    - Added mise.toml for task management with a serve command to run the application.
    - Implemented main application logic in src/main.rs with routes for a hello world response and an echo endpoint.
    - Created an empty model file src/lib/model.rs for future development.

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let url = "localhost";
+    let port = 5000;
+    println!("running on http://{}:{}", url, port);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind((url, port))?
+    .run()
+    .await
+}
