802815a09b49c605a675ea7e5219af64fd04157e
NabeKz <ayuzak4me38@gmail.com>
2025-06-03 08:51:06 +0900

    Refactor form handling; update form method to use POST and add support for PUT method in HTML forms

--- src/router/html.rs
+++ src/router/html.rs
@@ -1,28 +1,33 @@
 enum Method {
     POST,
+    PUT,
     DELETE,
 }
 
 fn form(method: Method, action: &str, content: String) -> String {
-    let req_method = match method {
-        Method::POST => "POST",
-        Method::DELETE => "DELETE",
+    let action = match method {
+        Method::PUT => action.to_owned() + "?_method=PUT",
+        Method::DELETE => action.to_owned() + "?_method=DELETE",
+        _ => action.to_owned(),
     };
 
     format!(
         "
-        <form action={} method={}>
+        <form action={} method=POST>
             {}
             <div>
                 <button type=submit> submit </button>
             </div>
         </form>
     ",
-        action, req_method, content
+        action, content
     )
 }
 pub fn post_form(action: &str, content: String) -> String {
     form(Method::POST, action, content)
+}
+pub fn put_form(action: &str, content: String) -> String {
+    form(Method::PUT, action, content)
 }
 pub fn delete_form(action: &str, content: String) -> String {
     form(Method::DELETE, action, content)
